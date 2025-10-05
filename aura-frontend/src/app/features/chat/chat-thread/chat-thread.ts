0bbe973cbab61ea0a8c29ff4bc23ad532e104c00
hamza.hannat <hamza.hannat@epita.fr>
2025-10-05 17:38:16 +0200

    feat(init) : initing frontend architecture

---
+++ aura-frontend/src/app/features/chat/chat-thread/chat-thread.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-thread',
+  imports: [],
+  templateUrl: './chat-thread.html',
+  styleUrl: './chat-thread.scss'
+})
+export class ChatThread {
+
+}
---
+++ aura-frontend/src/app/features/sidebar/sessions-sidebar/sessions-sidebar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sessions-sidebar',
+  imports: [],
+  templateUrl: './sessions-sidebar.html',
+  styleUrl: './sessions-sidebar.scss'
+})
+export class SessionsSidebar {
+
+}
